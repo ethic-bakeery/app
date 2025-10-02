13bc3baa88053a6360d499ce26c20ffbc0e43d8b
ethic-bakeery <cryfor419@gmail.com>
2025-10-02 16:24:06 -0400

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,21 @@
-import next from 'next/core-web-vitals'
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  ...next,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
       'react/no-unescaped-entities': 'off'
     }
   }
-]
+];
+
+export default eslintConfig;
